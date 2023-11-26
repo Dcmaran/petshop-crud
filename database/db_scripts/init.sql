35212d064fb66dd98e1f193222c3b5bf9664630c
Dcmaran <diogocmr2002@gmail.com>
2023-11-26 01:17:31 -0300

    Change animal names

--- database/db_scripts/init.sql
+++ database/db_scripts/init.sql
@@ -19,20 +19,20 @@ CREATE TABLE consulta (
 );
 
 INSERT INTO cliente (nome, telefone) VALUES
-    ('João Silva', '111-1111'),
-    ('Maria Santos', '222-2222'),
-    ('Carlos Oliveira', '333-3333'),
-    ('Ana Souza', '444-4444'),
-    ('Fernando Costa', '555-5555'),
-    ('Isabel Lima', '666-6666');
+    ('João Silva', '1111-1111'),
+    ('Maria Santos', '2222-2222'),
+    ('Carlos Oliveira', '3333-3333'),
+    ('Ana Souza', '4444-4444'),
+    ('Fernando Costa', '5555-5555'),
+    ('Isabel Lima', '6666-6666');
 
 INSERT INTO animal (nome, especie, dono_id) VALUES
-    ('Cachorro1', 'Cachorro', 1),
-    ('Gato1', 'Gato', 2),
-    ('Pássaro1', 'Pássaro', 3),
-    ('Cachorro2', 'Cachorro', 4),
-    ('Gato2', 'Gato', 2),
-    ('Pássaro2', 'Pássaro', 6);
+    ('Belinha', 'Cachorro', 1),
+    ('Alfredo', 'Gato', 2),
+    ('Amarelinho', 'Pássaro', 3),
+    ('Dengo', 'Cachorro', 4),
+    ('Mia', 'Gato', 2),
+    ('Piu Piu', 'Pássaro', 6);
 
 INSERT INTO consulta (data, animal_id, descricao) VALUES
     ('2023-01-01', 1, 'Banho e Tosa'),
